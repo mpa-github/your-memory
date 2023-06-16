e42a4b5b47dd66a397a2b550fe34881cb01402ec
Pavel Muravyev <muravyev.pavel@gmail.com>
2023-06-16 13:30:24 +0300

    Initial commit.

---
+++ src/test/java/com/mpa/yourmemory/YourMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpa.yourmemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourMemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
